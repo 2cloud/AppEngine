4c645a5c6f074efafe6ec1872edf9b7e05afe9b8
Paddy Foran <foran.paddy@gmail.com>
2011-07-12 22:42:29 -0400

    Fixed logic error that caused AddLink to think it was over quota when it wasn't, and vice-versa.

--- devlinks.py
+++ devlinks.py
@@ -103,7 +103,7 @@ class AddLinkPage(webapp.RequestHandler):
                 receiver = device
             link = models.LinkData(url=self.request.get('link'),
                 sender=device, receiver=receiver).save()
-            if models.getQuota().amount > models.getStats(
+            if models.getQuota().amount <= models.getStats(
                     'quota').count:
                 channel = channels.Channel(receiver.address, False)
                 channel.sendLink(link)
